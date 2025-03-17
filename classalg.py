735269d5ef947b069888586d6c8953809192c435
watsbron <waits.bryon@baxter-academy.org>
2025-03-17 10:00:20 -0400

    removed unnessary prints and repeated functions

--- classalg.py
+++ classalg.py
@@ -1,6 +1,8 @@
 import json
 import csv
 
+order = []
+
 def getClassList():
     with open('classdat.json') as json_file:
         data = json.load(json_file)
@@ -34,9 +36,6 @@ def class_weighting(cl, st):
 
     return score
 
-
-order = []
-
 def getTopicByIndex(class_index, topic_index):
     class_list = getClassList()  # Get dictionary of all classes
     class_keys = list(class_list.keys())  # Extract class names as a list
@@ -52,19 +51,6 @@ def getTopicByIndex(class_index, topic_index):
     else:
         return "Class index out of range"
 
-
-def getDataByIndex(index, typerequest = "topics"):
-    class_keys = list(getClassList())  # Get all class names as a list
-    if 0 <= index < len(class_keys):  # Ensure index is within range
-        class_name = class_keys[index]  # Get class name by index
-        if typerequest == "name":
-            return getClassList()[class_name]["name"]  # Return topics of the class
-        if typerequest == "topics":
-            return getClassList()[class_name]["Topics"]  # Return topics of the class
-        else:
-            print("Improper input, please input either \"name\" to get the name of a class or \"topics\" to get the topics in a class")
-    else:
-        return "Index out of range"
 
 def getDataByIndex(index, typerequest = "topics"):
     class_keys = list(getClassList())  # Get all class names as a list
